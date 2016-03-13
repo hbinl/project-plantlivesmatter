e93da8c9dde5fb98dc03e0c24401acc0720bb18d
Anson <anson_tio@yahoo.com>
2016-03-14 00:26:02 +0800

    Plant tree is now working, and major changes to GameControl Component

---
+++ Assets/Scripts/SpawnTreeUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnTreeUI : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Debug.Log("TREE UI");
+		if (GameControl.coinValue >= 100)
+		{
+			GameControl.canSpawnTree = true;
+		}
+	}
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour {
+
+	public TreeSci treeObject;
+	public bool occupied;
+
+	public GameControl gameControl;
+
+	void OnMouseDown()
+	{
+		Debug.Log("Tile Pressed");
+		if (GameControl.canSpawnTree)
+		{
+			GameControl.canSpawnTree = false;
+
+			GameControl.coinValue -= 100;
+
+			gameControl.SpawnTree(transform.position);
+
+			occupied = true;
+		}
+	}
+
+}
