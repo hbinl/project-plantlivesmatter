ab3ba109c7e734b437d57dd06e162e72defdc8cc
Hao Bin <hbloh1@student.monash.edu>
2016-04-03 22:18:28 +0800

    Added skeleton game store scene, world tree, and linking

--- Assets/Scripts/Intro/IntroSceneButton.cs
+++ Assets/Scripts/Intro/IntroSceneButton.cs
@@ -13,4 +13,23 @@ public class IntroSceneButton : MonoBehaviour {
 	{
 		SceneManager.LoadScene ("LoginScene");
 	}
+    
+    public void onClickLoadStore() 
+    {
+        SceneManager.LoadScene("GameStoreScene");
+    }
+    
+    public void onClickLoadHighScore() 
+    {
+        SceneManager.LoadScene("HighScoreScene");
+    }
+    
+    public void onClickLoadWorldTree() 
+    {
+        SceneManager.LoadScene("WorldTreeScene");
+    }
+    
+    public void onClickLoadHome() {
+        SceneManager.LoadScene("IntroScene");
+    }
 }
